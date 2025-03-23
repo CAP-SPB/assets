17685a08ffc555b35630477ea7dd47d09de0ddd2
Konstantin Stanislavovich Lipinskiy <kastentop2005@gmail.com>
2025-03-23 12:59:18 +0300

    Push some changes

--- Scripts/Post-Install.ps1
+++ Scripts/Post-Install.ps1
@@ -1,10 +1,9 @@
-<#
 #requires -version 5
-
+<#
 .SYNOPSIS
   Скрипт пост-настройки
 .DESCRIPTION
-  Скрипт пост-настройки системы Windows на компьютерах Колледжа электроники и приборостроения
+  Скрипт пост-настройки системы Windows на компьютерах Колледжа электроники и информационных технологий
 .INPUTS
   None
 .OUTPUTS
@@ -16,18 +15,40 @@
   Purpose/Change: Первая версия
 #>
 
-# Функция для проверки интернет-соединения
+# Глобальная переменная для хранения состояния интернет-соединения
+$global:isInternetAvailable = Test-InternetConnection
+
+function Init {
+  # Инициализация скрипта
+  Write-Host "Начало выполнения скрипта..."
+
+  if ($global:isInternetAvailable) {
+    try {
+      Update-AllPackages
+      Install-Packages
+      Resize-TitleBar
+      Resize-ScrollBar
+      Increase-WallpaperQuality
+      Enable-VerboseMessages
+    } catch {
+      Write-Error "Произошла ошибка во время выполнения скрипта: $_"
+    }
+  }
+}
+
+# Тест соединения
 function Test-InternetConnection {
   param (
       [string]$remote = "ya.ru"
   )
+
   try {
       $pingResult = Test-Connection -ComputerName $remote -Count 3 -Quiet
       if ($pingResult) {
-          Write-Output "Подключение к интернету установлено!"
+          Write-Host "Подключение к интернету установлено!"
           return $true
       } else {
-          Write-Output "Нет подключения к инетрнету. Невозможно продолжить!"
+          Write-Host "Нет подключения к интернету. Невозможно продолжить!"
           return $false
       }
   } catch {
@@ -36,44 +57,132 @@ function Test-InternetConnection {
   }
 }
 
-# Функция для обновления всех пакетов через winget
+# Обновление всех пакетов
 function Update-AllPackages {
-  if (Test-InternetConnection) {
-      try {
-          Write-Output "Начало обновления приложений..."
-          winget upgrade --all
-          Write-Output "Все приложения были обновлены!"
-      } catch {
-          Write-Error "Произошла ошибка во время обновления: $_"
-        }
-      } else {
-      Write-Output "Невозможно продолжить из-за отсутствия подключения к инетрнету!"
+  try {
+    Write-Host "Начало обновления приложений..."
+    winget upgrade --all
+    Write-Host "Все приложения были обновлены!"
+    return $true
+  } catch {
+    throw "Произошла ошибка во время обновления: $_"
   }
 }
 
-# Функция для установки пакетов
+# Установка необходимых пакетов
 function Install-Packages {
   $tempWingetFile = [System.IO.Path]::Combine($env:TEMP, "winget-packages.json")
   $wingetPkgListUrl = "https://raw.githubusercontent.com/CAP-SPB/assets/refs/heads/main/Config/winget-packages.json"
 
-  if (Test-InternetConnection) {
-    try {
-      Invoke-WebRequest -Uri $wingetPkgListUrl -OutFile $tempWingetFile
-      Write-Output "Начало установки приложений..."
-      winget import $tempWingetFile
-      Write-Output "Все приложения были установлены!"
-      Remove-Item -Path $tempWingetFile
+  try {
+    Invoke-WebRequest -Uri $wingetPkgListUrl -OutFile $tempWingetFile
+    Write-Host "Начало установки приложений..."
+    winget import $tempWingetFile
+    Write-Host "Все приложения были установлены!"
+    Remove-Item -Path $tempWingetFile
+    return $true
   } catch {
-      Write-Error "An error occurred while downloading or processing the file: $_"
-    }
+    throw "Произошла ошибка при загрузке или обработке файла: $_"
   }
 }
 
-# Функция для кастомизации интерфейса
-function Customize {
+# Уменьшить размер заголовков окон
+function Resize-TitleBar {
+  try {
+    New-ItemProperty -Path 'HKCU:\Control Panel\Desktop\WindowMetrics' -Name 'CaptionHeight' -Value '-270' -PropertyType DWORD -Force
+    Write-Host "Размер заголовка окна изменён"
+    return $true
+  } catch {
+    throw "Не удалось изменить размер заголовка окна: $_"
+  }
+}
+
+# Уменьшить размер полос прокрутки
+function Resize-ScrollBar {
+  try {
+    New-ItemProperty -Path 'HKCU:\Control Panel\Desktop\WindowMetrics' -Name 'ScrollWidth' -Value '-210' -PropertyType DWORD -Force
+    Write-Host "Размер полос прокрутки изменён"
+    return $true
+  } catch {
+    throw "Не удалось изменить размер полос прокрутки: $_"
+  }
+}
+
+# Отключение сжатия обоев
+function Increase-WallpaperQuality {
+  try {
+    New-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name 'JPEGImportQuality' -Value '100' -PropertyType DWORD -Force
+    Write-Host "Сжатие обоев отключено"
+    return $true
+  } catch {
+    throw "Не удалось отключить сжатие обоев: $_"
+  }
+}
+
+# Подробные сообщения загрузки и выключения
+function Enable-VerboseMessages {
+  try {
+    New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name 'verbosestatus' -Value '1' -PropertyType DWORD -Force
+    Write-Host "Подробные сообщения загрузки включены"
+    return $true
+  } catch {
+    throw "Не удалось включить подробные сообщения о загрузке: $_"
+  }
+}
+
+# Групповые политики
+function Group-Policy {
+  # Установка обоев
+  try {
+    $wallpaperUrl = "https://github.com/CAP-SPB/assets/raw/refs/heads/main/Images/CapLogo.bmp"
+    $wallpaperPath = "$env:ProgramData\CapWallpaper.bmp"
+  
+    Invoke-WebRequest -Uri $wallpaperUrl -OutFile $wallpaperPath
+    Set-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\Windows\Personalization' -Name 'DesktopWallpaper' -Value $wallpaperPath -Force
+    New-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\Windows\Personalization' -Name 'DesktopWallpaperStyle' -Value 2 -PropertyType DWORD -Force
+    RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+  } catch {
+    throw "Не удалось установить обои: $_"
+  }
+
+  # Запретить Microsoft Store
+  try {
+    Set-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\WindowsStore' -Name 'RemoveWindowsStore' -Value 1 -Type DWORD
+  } catch {
+    throw "Не удалось запретить Microsoft Store: $_"
+  }
+
+  # Запретить посещение сайтов
+  try {
+  $domainsToBlock = @(
+    "store.steampowered.com",
+    "store.epicgames.com",
+    "heroicgameslauncher.com",
+    "playnite.link",
+    "minecraft.net",
+    "ubisoft.com",
+    "ea.com",
+    "download.battle.net",
+    "vkplay.ru",
+    "prismlauncher.org",
+    "atlauncher.com"
+  )
+
+  $hostsFilePath = "C:\Windows\System32\drivers\etc\hosts"
+
+  foreach ($domain in $domainsToBlock) {
+    Add-Content -Path $hostsFilePath -Value "127.0.0.1 `t $domain"
+}
+  ipconfig /flushdns
+  } catch {
+    throw ("Произошла ошибка записи в hosts: $_")
+  }
+}
+
+# Политики AppLocker
+function AppLocker-Policy {
 
 }
 
-# Вызов функций
-Update-AllPackages
-Install-Packages
+# Главная функция
+Initialize
